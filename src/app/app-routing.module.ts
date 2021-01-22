a3b8a4e808ff6677d1734302e2e54ee591f7c24d
CeAlCuadrado <cealcuadrado@outlook.com>
2021-01-21 22:36:05 -0300

    Primer diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { HomeComponent } from './home/home.component';
+import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.component';
+import { HeaderComponent } from './layouts/basic-layout/header/header.component';
+import { FooterComponent } from './layouts/basic-layout/footer/footer.component';
+import { MainComponent } from './basic-layout/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
