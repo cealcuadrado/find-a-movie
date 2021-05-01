bee8e5da97a1b6357137e1dff6e6e54c5a738869
CeAlCuadrado <cealcuadrado@outlook.com>
2021-05-01 15:43:12 -0400

    Agregado contenedor para las IDs no asociadas a una película en la vista Movie

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,8 +1,10 @@
+import { MovieTrailerComponent } from './movie-trailer/movie-trailer.component';
+import { CastAndCrewComponent } from './cast-and-crew/cast-and-crew.component';
 import { environment } from './../../environments/environment';
 import { MovieService } from './movie.service';
 import { MovieDetail } from './../shared/interfaces/movie-detail';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-movie',
@@ -10,11 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent implements OnInit {
+  public loading = true;
   public movieDetail: MovieDetail;
   public id: string;
   private posterUrl: string = environment.posterUrl;
   private backdropUrl: string = environment.backdropUrl;
   private imdbUrl: string = environment.imdbUrl;
+
+  @ViewChild(CastAndCrewComponent, { static: true }) castAndCrew: CastAndCrewComponent;
+  @ViewChild(MovieTrailerComponent, { static: true }) movieTrailer: MovieTrailerComponent;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,16 +28,33 @@ export class MovieComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    console.log('ngOnInit movie')
+    this.getMovie();
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges() movie');
+    this.loading = true;
+    this.getMovie();
+  }
+
+  getMovie(): void {
+    console.log('getMovie()');
     this.activatedRoute.params.subscribe((params) => {
-      this.id = params.id;
-      this.getDetails();
+      if (params.id) {
+        this.id = params.id;
+        this.getDetails();
+      }
     });
   }
 
   getDetails(): void {
+    console.log('getDetails()');
     this.movie.getMovieDetail(this.id).subscribe((detail) => {
-      console.log(detail);
-      this.movieDetail = detail;
+      if (Object.values(detail).length > 0) {
+        this.movieDetail = detail;
+      }
+      this.loading = false;
     });
   }
 
