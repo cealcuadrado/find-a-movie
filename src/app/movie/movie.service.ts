02fc5f0caab7ef39aa56601184da4c099bbbe83f
CeAlCuadrado <cealcuadrado@outlook.com>
2021-05-01 14:49:27 -0400

    Añadida gestión general de errores en el servicio movie

--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -1,26 +1,38 @@
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { CastCrewResult } from './../shared/interfaces/cast-crew-result';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { MovieDetail } from '../shared/interfaces/movie-detail';
-import { CastCrewResult } from '../shared/interfaces/cast-crew-result';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
   private url = environment.apiUrl;
   private key = environment.apiKey;
   private language = environment.language;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMovieDetail(id: string): Observable<MovieDetail> {
-    return this.http.get<MovieDetail>(`${this.url}/movie/${id}?api_key=${this.key}&language=${this.language}`);
+    return this.http.get<MovieDetail>(
+      `${this.url}/movie/${id}?api_key=${this.key}&language=${this.language}`
+    ).pipe(
+      catchError((error: any): Observable<any> => {
+        return of({});
+      })
+    );
   }
 
   getCastAndCrew(id: string): Observable<CastCrewResult> {
-    return this.http.get<CastCrewResult>(`${this.url}/movie/${id}/credits?api_key=${this.key}&language=${this.language}`)
+    return this.http.get<CastCrewResult>(
+        `${this.url}/movie/${id}/credits?api_key=${this.key}&language=${this.language}`
+    ).pipe(
+      catchError((error: any): Observable<any> => {
+        return of({});
+      })
+    );
   }
 }
