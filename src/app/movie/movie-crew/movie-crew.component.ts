80b231141a45f574fd50342df8ba19137b6af837
CeAlCuadrado <cealcuadrado@outlook.com>
2021-11-18 19:03:03 -0300

    Agregadas filas ExecutiveProducers y Cinematographers al componente Crew

--- src/app/movie/movie-crew/movie-crew.component.ts
+++ src/app/movie/movie-crew/movie-crew.component.ts
@@ -1,14 +1,14 @@
 import { PersonLink } from './../../shared/interfaces/person-link';
 import { Component, Input, OnInit } from '@angular/core';
 import { Crew } from 'src/app/shared/interfaces/crew';
+import { MergeMapSubscriber } from 'rxjs/internal/operators/mergeMap';
 
 @Component({
   selector: 'app-movie-crew',
   templateUrl: './movie-crew.component.html',
-  styleUrls: ['./movie-crew.component.scss']
+  styleUrls: ['./movie-crew.component.scss'],
 })
 export class MovieCrewComponent implements OnInit {
-
   public loading: boolean = true;
 
   @Input() crew: Crew[] = [];
@@ -18,11 +18,13 @@ export class MovieCrewComponent implements OnInit {
   public screenPlayers: PersonLink[] = [];
   public storyPlayers: PersonLink[] = [];
   public producers: PersonLink[] = [];
+  public executiveProducers: PersonLink[] = [];
+  public cinematographers: PersonLink[] = [];
 
   public isBasedOnWork: boolean = false;
   public basedOnWorkAuthors: PersonLink[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.setCrew();
@@ -36,6 +38,8 @@ export class MovieCrewComponent implements OnInit {
     this.getStoryPlayers();
     this.getBasedOnWork();
     this.getProducers();
+    this.getExecutiveProducers();
+    this.getCinematographers();
   }
 
   private getDirection(): void {
@@ -43,7 +47,7 @@ export class MovieCrewComponent implements OnInit {
       .filter(member => member.job == 'Director')
       .map(member => {
         return { name: member.name, id: member.id };
-      })
+      });
   }
 
   private getWriting(): void {
@@ -51,7 +55,7 @@ export class MovieCrewComponent implements OnInit {
       .filter(member => member.job === 'Writer')
       .map(member => {
         return { name: member.name, id: member.id };
-    })
+      });
   }
 
   private getScreenPlayers(): void {
@@ -59,30 +63,30 @@ export class MovieCrewComponent implements OnInit {
       .filter(member => member.job === 'Screenplay')
       .map(member => {
         return { name: member.name, id: member.id };
-      })
+      });
   }
 
   private getStoryPlayers(): void {
     this.storyPlayers = this.crew
-      .filter(member => member.job === 'Story' )
+      .filter(member => member.job === 'Story')
       .map(member => {
         return { name: member.name, id: member.id };
-      })
+      });
   }
 
   private getBasedOnWork(): void {
     let basedOnWorkAuthors = this.crew
       .filter(member => {
-        return member.job === 'Novel' || member.job === 'Author'
+        return member.job === 'Novel' || member.job === 'Author';
       })
       .map(member => {
-        return { name: member.name, id: member.id }
+        return { name: member.name, id: member.id };
       });
 
     if (basedOnWorkAuthors.length > 0) {
       this.isBasedOnWork = true;
       this.basedOnWorkAuthors = basedOnWorkAuthors;
-    }  else {
+    } else {
       this.isBasedOnWork = false;
     }
   }
@@ -91,7 +95,23 @@ export class MovieCrewComponent implements OnInit {
     this.producers = this.crew
       .filter(member => member.job === 'Producer')
       .map(member => {
-        return { name: member.name, id: member.id}
+        return { name: member.name, id: member.id };
+      });
+  }
+
+  private getExecutiveProducers(): void {
+    this.executiveProducers = this.crew
+      .filter(member => member.job === 'Executive Producer')
+      .map(member => {
+        return { name: member.name, id: member.id };
+      });
+  }
+
+  private getCinematographers(): void {
+    this.cinematographers = this.crew
+      .filter(member => member.job === 'Director of Photography')
+      .map(member => {
+        return { name: member.name, id: member.id }
       });
   }
 }
