255204aa69c7cd0448f30b6263bed66f13dc9ee5
CeAlCuadrado <cealcuadrado@outlook.com>
2021-11-25 19:44:25 -0300

    Creadas secciones CastSummary y CrewSummary en componente PersonOverview

--- src/app/person/person-overview/person-overview.component.ts
+++ src/app/person/person-overview/person-overview.component.ts
@@ -1,5 +1,7 @@
+import { CastCredit } from './../../shared/interfaces/cast-credit';
 import { PersonDetail } from './../../shared/interfaces/person-detail';
 import { Component, Input, OnInit } from '@angular/core';
+import { CrewCredit } from 'src/app/shared/interfaces/crew-credit';
 
 @Component({
   selector: 'app-person-overview',
@@ -8,11 +10,27 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PersonOverviewComponent implements OnInit {
 
+  loading = true;
+
   @Input() personDetail: PersonDetail;
+  @Input() castCredits: CastCredit[];
+  @Input() crewCredits: CrewCredit[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setCastAndCrew();
   }
 
+  ngOnChanges(): void {
+    this.setCastAndCrew();
+  }
+
+  public setCastAndCrew(): void {
+    this.loading = false;
+    this.castCredits = this.castCredits.splice(0, 5);
+    this.crewCredits = this.crewCredits.splice(0, 5);
+    console.log(this.castCredits);
+    console.log(this.crewCredits);
+  }
 }
