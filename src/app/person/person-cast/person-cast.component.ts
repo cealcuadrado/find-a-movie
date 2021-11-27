6f2f830ac0ed7f4b62113fc13fff848104f5ee87
CeAlCuadrado <cealcuadrado@outlook.com>
2021-11-26 21:29:32 -0300

    Resultados de buscar créditos de reparto pasados a componente PersonCast

--- src/app/person/person-cast/person-cast.component.ts
+++ src/app/person/person-cast/person-cast.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CastCredit } from './../../shared/interfaces/cast-credit';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-person-cast',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonCastComponent implements OnInit {
 
+  loading = true;
+
+  @Input() castCredits: CastCredit[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setCast();
+  }
+
+  ngOnChanges(): void {
+    this.setCast();
+  }
+
+  public setCast(): void {
+    this.loading = false;
   }
 
 }
