461f80a01ae2fabfca83c2610e276c230e3a6ea2
CeAlCuadrado <cealcuadrado@outlook.com>
2021-11-30 19:41:12 -0300

    Simplificado layout de pestaña Cast de Person para agregar input de filtro

--- src/app/person/person-cast/person-cast.component.ts
+++ src/app/person/person-cast/person-cast.component.ts
@@ -11,12 +11,9 @@ export class PersonCastComponent implements OnInit {
 
   public currentPage: number;
   public resultsPerPage = 20;
-
   public totalResults: number;
-  public totalPages: number;
 
-  public pageNumbers: number[];
-  public selectPage: number;
+  public filterInput: string;
 
   @Input() name: string = '';
   @Input() castCredits: CastCredit[];
@@ -33,37 +30,12 @@ export class PersonCastComponent implements OnInit {
 
   public setCast(): void {
     this.currentPage = 1;
-    this.selectPage = 1;
     this.loading = false;
     this.totalResults = this.castCredits.length;
-    this.totalPages = Math.ceil(this.castCredits.length / this.resultsPerPage);
-    this.generateSelectPageNumbers();
-  }
-
-  public calculateLeftCounter(): number {
-    return this.resultsPerPage * (this.currentPage - 1) + 1;
-  }
-
-  public calculateRightCounter(): number {
-    return this.resultsPerPage * this.currentPage < this.totalResults
-      ? this.resultsPerPage * this.currentPage
-      : this.totalResults;
-  }
-
-  private generateSelectPageNumbers(): void {
-    this.pageNumbers = [];
-    for (let i = 0; i < this.totalPages; i++) {
-      this.pageNumbers.push(i + 1);
-    }
-  }
-
-  public changeSelectPage(event: any) {
-    this.onPageChange(event.target.value);
   }
 
   public onPageChange(event: any): void {
     this.currentPage = event;
-    this.selectPage = this.currentPage;
     this.window.scrollTo({ top: 400 });
   }
 }
