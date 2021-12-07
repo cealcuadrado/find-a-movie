a210a01af99131b391272ba6d78d97a378daacf1
CeAlCuadrado <cealcuadrado@outlook.com>
2021-12-07 20:32:26 -0300

    Limpieza de TS del componente Header

--- src/app/layouts/basic-layout/header/header.component.ts
+++ src/app/layouts/basic-layout/header/header.component.ts
@@ -1,8 +1,4 @@
-import { MovieListResult } from './../../../shared/interfaces/movie-list-result';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
