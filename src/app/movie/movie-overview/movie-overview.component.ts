df5a5eeb1037e0b90a4b7ed1b94028521999ed04
CeAlCuadrado <cealcuadrado@outlook.com>
2021-12-16 20:28:19 -0300

    Cambio de sistema de pestañas en Movie por sistema Routing

--- src/app/movie/movie-overview/movie-overview.component.ts
+++ src/app/movie/movie-overview/movie-overview.component.ts
@@ -1,3 +1,7 @@
+import { Subscription } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+import { MovieService } from './../movie.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MovieDetail } from './../../shared/interfaces/movie-detail';
 import { Component, Input, OnInit } from '@angular/core';
 import { Cast } from 'src/app/shared/interfaces/cast';
@@ -10,15 +14,103 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./movie-overview.component.scss'],
 })
 export class MovieOverviewComponent implements OnInit {
+  public loadingDetails = true;
+  public loadingCastAndCrew = true;
 
-  @Input() id: string;
-  @Input() movieDetail: MovieDetail;
-  @Input() cast: Cast[];
-  @Input() crew: Crew[];
+  public id: string;
+  public movieDetail: MovieDetail;
+  public cast: Cast[] = [];
+  public crew: Crew[] = [];
 
   private imdbUrl: string = environment.imdbUrl;
 
-  constructor() {}
+  private activatedRouteSubscription: Subscription | undefined;
+  private movieDetailSubscription: Subscription;
+  private movieCastAndCrewSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private movie: MovieService,
+    private titleService: Title
+  ) {}
+
+  ngOnInit(): void {
+    this.loadingDetails = true;
+    this.loadingCastAndCrew = true;
+    this.setOverview();
+  }
+
+  ngOnChanges(): void {
+    this.loadingDetails = true;
+    this.loadingCastAndCrew = true;
+    this.setOverview();
+  }
+
+  private setOverview(): void {
+    this.activatedRouteSubscription = this.activatedRoute.parent?.params.subscribe(
+      (params) => {
+        console.log(params);
+        if (params.id) {
+          this.id = params.id;
+          this.getDetails();
+          this.getCastAndCrew();
+        }
+      }
+    );
+  }
+
+  private getDetails(): void {
+    this.movieDetailSubscription = this.movie
+      .getMovieDetail(this.id)
+      .subscribe((detail) => {
+        console.log(detail);
+        if (Object.values(detail).length > 0) {
+          this.movieDetail = detail;
+          this.setWindowTitle();
+        }
+        this.loadingDetails = false;
+      });
+  }
+
+  private getCastAndCrew(): void {
+    this.movieCastAndCrewSubscription = this.movie
+      .getCastAndCrew(this.id)
+      .subscribe((result) => {
+        console.log(result);
+        this.cast = result.cast;
+        this.crew = result.crew;
+        this.loadingCastAndCrew = false;
+      });
+  }
+
+  public loading(): boolean {
+    return this.loadingDetails || this.loadingCastAndCrew;
+  }
+
+  private setWindowTitle(): void {
+    let date = new Date(this.movieDetail.release_date);
+    let detail = this.movieDetail;
+    this.titleService.setTitle(
+      `${this.setLocalOrForeignTitle(
+        detail
+      )} (${date.getFullYear()}) | Find a Movie`
+    );
+  }
+
+  private setLocalOrForeignTitle(detail: MovieDetail): string {
+    return !detail.original_language.match('en') &&
+      !detail.original_title.match(detail.title)
+      ? `${detail.title} (${detail.original_title})`
+      : detail.title;
+  }
+
+  ngOnDestroy() {
+    if (this.activatedRouteSubscription) {
+      this.activatedRouteSubscription.unsubscribe();
+    }
+
+    this.movieDetailSubscription.unsubscribe();
+    this.movieCastAndCrewSubscription.unsubscribe();
+  }
 }
