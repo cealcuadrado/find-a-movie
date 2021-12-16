182e93c66cebd517b2cd5ade3f4f20d9e7d3f356
CeAlCuadrado <cealcuadrado@outlook.com>
2021-12-16 20:36:47 -0300

    Ruteado MovieCast

--- src/app/movie/movie-cast/movie-cast.component.ts
+++ src/app/movie/movie-cast/movie-cast.component.ts
@@ -1,5 +1,10 @@
+import { Title } from '@angular/platform-browser';
+import { MovieService } from './../movie.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Component, OnInit, Input } from '@angular/core';
 import { Cast } from 'src/app/shared/interfaces/cast';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 
 @Component({
   selector: 'app-movie-cast',
@@ -10,9 +15,17 @@ export class MovieCastComponent implements OnInit {
 
   public loading: boolean = true;
 
-  @Input() cast: Cast[] = [];
+  public id: string;
+  public cast: Cast[] = [];
 
-  constructor() { }
+  private activatedRouteSubscription: Subscription | undefined;
+  private movieCastSubscription: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movie: MovieService,
+    private titleService: Title
+  ) { }
 
   ngOnInit(): void {
     this.setCast();
@@ -23,6 +36,23 @@ export class MovieCastComponent implements OnInit {
   }
 
   private setCast(): void {
-    this.loading = false;
+    this.activatedRouteSubscription = this.activatedRoute.parent?.params.subscribe(
+      params => {
+        if (params.id) {
+          this.id = params.id;
+          this.getCast();
+        }
+      }
+    );
+  }
+
+  private getCast(): void {
+    this.movieCastSubscription = this.movie.getCastAndCrew(this.id).subscribe(result => {
+      this.cast = result.cast;
+    });
+  }
+
+  ngOnDestroy() {
+    this.movieCastSubscription.unsubscribe();
   }
 }
