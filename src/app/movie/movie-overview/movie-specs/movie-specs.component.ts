bb4b2149310a58b6505fce193b35b12c9c77d581
CeAlCuadrado <cealcuadrado@outlook.com>
2022-01-27 17:20:34 -0300

    Creado método initMovieSpecs() para MovieSpecs

--- src/app/movie/movie-overview/movie-specs/movie-specs.component.ts
+++ src/app/movie/movie-overview/movie-specs/movie-specs.component.ts
@@ -6,34 +6,38 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-movie-specs',
   templateUrl: './movie-specs.component.html',
-  styleUrls: ['./movie-specs.component.scss']
+  styleUrls: ['./movie-specs.component.scss'],
 })
 export class MovieSpecsComponent implements OnInit {
 
   public loading: boolean = true;
-  public originalLanguage: Language;
 
   @Input() movieDetail: MovieDetail;
 
+  public originalLanguage: Language;
+
   constructor(
     private localStorageService: LocalStorageService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.loading = false;
-    console.log(this.movieDetail);
-    this.setOriginalLanguage();
+    this.initMovieSpecs();
   }
 
   ngOnChanges() {
-    this.loading = false;
+    this.initMovieSpecs();
+  }
+
+  private initMovieSpecs(): void {
+    this.loading = true;
     this.setOriginalLanguage();
   }
 
   private setOriginalLanguage(): void {
     const languages = this.localStorageService.get('languages');
     this.originalLanguage = languages.filter((language: Language) => {
-      return language.iso_639_1 === this.movieDetail.original_language
+      return language.iso_639_1 === this.movieDetail.original_language;
     })[0];
+    this.loading = false;
   }
 }
